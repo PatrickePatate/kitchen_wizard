9157685cf55a3673458fc64aa7b4d0803fa306af
PatrickePatate <lucienpuget1@gmail.com>
2024-11-24 13:11:47 +0100

    Small fix

--- init-project.sh
+++ init-project.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir -p /app/storage/{app/public,framework/{cache,sessions,testing,views},logs}
 sleep 5
+composer install --no-dev --no-interaction --optimize-autoloader
 php /app/artisan migrate --force
 php /app/artisan optimize
 php /app/artisan storage:link
