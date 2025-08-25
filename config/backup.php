292d0e330f05e8cc5d3ecc2e8e45861800f18e10
Lucien <lucienpuget1@gmail.com>
2025-08-25 21:12:37 +0200

    Fuck spatie/backup

--- config/backup.php
+++ config/backup.php
@@ -214,8 +214,8 @@ return [
             'to' => config('app.backups.admin_email', ''),
 
             'from' => [
-                'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-                'name' => env('MAIL_FROM_NAME', 'Example'),
+                'address' => env('MAIL_FROM_ADDRESS', ''),
+                'name' => env('MAIL_FROM_NAME', 'Backups'),
             ],
         ],
 
